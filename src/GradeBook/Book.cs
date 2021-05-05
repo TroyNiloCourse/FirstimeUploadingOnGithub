d4520631eeaca0b3d16b8005d9110cbe9a304bd3
TroyNilo <alllose2troy@gmail.com>
2021-05-05 09:04:10 +0800

    Adding if/else & do while loop

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -14,7 +14,14 @@ namespace GradeBook
        
         public void Addgrade(double grade)  //void - not returning a value
         {
+            if(grade <= 100 && grade >= 0)
+            {
             grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Value");
+            }
         }
           public Statistics GetStatistics()
         {
@@ -23,16 +30,26 @@ namespace GradeBook
             result.Average =  0.0;
             result.High = double.MinValue;
             result.Low = double.MaxValue;
+            var i = 0;
+                do{
+                result.Low = Math.Min(grades[i], result.Low );  
+                result.High = Math.Max(grades[i], result.High);
+                result.Average += grades[i];
+                i++;
+                }
+                while(i < grades.Count);
+            
+                 result.Average /= grades.Count; // or result = result / grades.count  
+                 return result;
 
-
-            foreach(var grade in grades)
-            {
-                result.Low = Math.Min(grade, result.Low );  
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
-            }
-            result.Average /= grades.Count; // or result = result / grades.count  
-            return result;
+            // foreach(var grade in grades)
+            // {
+            //     result.Low = Math.Min(grade, result.Low );  
+            //     result.High = Math.Max(grade, result.High);
+            //     result.Average += grade;
+            // }
+            // result.Average /= grades.Count; // or result = result / grades.count  
+            // return result;
         }
         // public void ChangeName (string name)
         // {
